becba89e1fd476daa876c2b605c55d578d8641ba
rongbao.mo <rongbao.mo@allride.ai>
2024-07-05 10:13:56 +0800

    [mod]初始化项目

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"dev-module-3g8dv9dob670ce47","alias":"dev-module"}]
+const isMac = true
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.sh
@@ -0,0 +1,1 @@
+"/Applications/wechatwebdevtools.app/Contents/MacOS/cli" cloud functions deploy --e dev-module-3g8dv9dob670ce47 --n quickstartFunctions --r --project "/Users/allride/WeChatProjects/driver-school" --report_first --report
