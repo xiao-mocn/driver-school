53d6fb42cdcd478b0d9d92896c59b6ff5d35149e
rongbao.mo <rongbao.mo@allride.ai>
2024-07-19 17:12:18 +0800

    [mod]教练主页

--- miniprogram/pages/coach/home/index.js
+++ miniprogram/pages/coach/home/index.js
@@ -1,65 +1,49 @@
+//获取应用实例
 Page({
-
+  /**
+   * 页面的初始数据
+   */
   data: {
-    logo: '',
-    name: '',
-    is_login: true,
-    angle: 0,
   },
 
-  onLoad: function(options) {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
   },
-  //首次点击允许获取用户信息并且授权
-  getUserInfo: function (e) {
-    // var that = this
-    // if(e.detail.userInfo != undefined){
-    //   var userInfo = e.detail.userInfo
-    //   wx.setStorageSync('userInfo', userInfo);
-    //   wx.u.getUserInfo().then(res1 => {
-    //     var bmobUser = res1.result;
-    //     if (bmobUser.avatarurl == '' || bmobUser.avatarurl == undefined) {
-    //       wx.u.changeUserInfo(userInfo.avatarUrl, userInfo.nickName).then(res2 => { });
-    //     }
-    //     this.showlogin(false);
-    //     wx.setStorageSync('userInfo', res1.result)
-    //     wx.reLaunch({
-    //       url: '/pages/_index/index/index'
-    //     })
-    //     that.setData({
-    //       userInfo: res1.result,
-    //     })
-    //   })
-    // }
-  },
-  onReady: function() {
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
 
   },
 
-  onShow: function() {
-    // var that = this
-    // wx.getUserInfo({
-    //   success(res) {
-    //     wx.u.getUserInfo().then(res1 => {
-    //       console.log("数据",res1)
-    //       var bmobUser = res1.result;
-    //       if (bmobUser.avatarUrl == '' || bmobUser.avatarUrl == undefined) {
-    //         wx.u.changeUserInfo(res.userInfo.avatarUrl, res.userInfo.nickName).then(res2 => { });
-    //       }
-    //       wx.setStorageSync('userInfo', res1.result)
-    //       that.setData({
-    //         userInfo: res1.result,
-    //         finish: true,
-    //       })
-    //     })
-    //   }
-    // })
-  },
-  goSign: function (e) {
-    const type = e.currentTarget.dataset.type
-    console.log(type)
-    // wx.reLaunch({
-    //   url: '/pages/_index/index/index'
-    // })
-  }
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
 
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
 })
